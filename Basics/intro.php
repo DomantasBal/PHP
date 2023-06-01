4b644c7166ad3e1e11f8b760df7571e66ec9cbbb
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-06-01 12:11:27 +0300

    associative arrays

--- Basics/intro.php
+++ Basics/intro.php
@@ -16,8 +16,7 @@
     $name = "Domantas ";
     $number = 123;
     echo $name . $number;
-
-
+    // CONCATENATION OF VARIABLES
     $list = "<ul>" . "<li>" . $number .  "</li>" .  "</ul>";
     echo $list;
     ?>
@@ -25,19 +24,20 @@
 
     <!-- ARRAYS -->
     <h2>Arrays</h2>
-
     <?php
     $numbersList = array(123, 132, 21, 312, 321,);
     $array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     echo $numbersList;
     echo $numbersList[0];
     echo $array[3];
-
-    // print_r($array);
-
-
     ?>
 
+    <!-- ASSOCIATIVE ARRAYS -->
+    <h2>Associative arrays</h2>
+    <?php
+    $asocArray = ["first" => 1, "second" => " This is second element in the array"];
+    echo $asocArray["second"];
+    ?>
 
 
 
