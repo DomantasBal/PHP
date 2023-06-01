c2fbba85f2dce7658ad9430f48d2d0e75cfcd42f
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-06-01 13:07:08 +0300

    checking functions

--- Basics/functions.php
+++ Basics/functions.php
@@ -1,12 +1,24 @@
 <?php
+
+// Functions
+wowzers();
+
+
 function wowzers()
 {
     echo "This is a function executed";
     echo "<br>";
 }
 
+wowzers();
 
-wowzers();
-wowzers();
-wowzers();
-wowzers();
+
+// Function parameters
+
+function calc($param1, $param2)
+{
+    return $param1 + $param2;
+}
+
+$result = calc(8, 2);
+echo $result;
