ae539dbd3b32cb3f4fbe801ffa96eba9d0f9a594
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-06-01 13:02:06 +0300

    checking functions syntax differences

---
+++ Basics/functions.php
@@ -0,0 +1,12 @@
+<?php
+function wowzers()
+{
+    echo "This is a function executed";
+    echo "<br>";
+}
+
+
+wowzers();
+wowzers();
+wowzers();
+wowzers();
--- Basics/intro.php
+++ Basics/intro.php
@@ -16,6 +16,7 @@
     $name = "Domantas ";
     $number = 123;
     echo $name . $number;
+
     // CONCATENATION OF VARIABLES
     $list = "<ul>" . "<li>" . $number .  "</li>" .  "</ul>";
     echo $list;
@@ -38,6 +39,63 @@
     $asocArray = ["first" => 1, "second" => " This is second element in the array"];
     echo $asocArray["second"];
     ?>
+
+    <!-- CONDITIONALS -->
+    <h2>Conditionals</h2>
+
+    <?php
+
+    if (3 < 10) {
+        echo "Yeep" . "<br>";
+    } else {
+        echo "Nope" . "<br>";
+    }
+
+
+    $expression = 33;
+    // $expression = 3;
+    switch ($expression) {
+        case 1:
+            echo "case 1";
+            break;
+        case 2:
+            echo "case 2";
+            break;
+        case 3:
+            echo "case 3";
+            break;
+
+        default:
+            echo "no cases present with this value";
+    }
+
+    ?>
+
+    <h2>Loops</h2>
+    <?php
+
+    // WHILE
+    $counter = 0;
+    while (10 > $counter) {
+        echo "Counter is " . $counter . "<br>";
+        $counter++;
+    }
+
+    // FOR 
+    for ($i = 0; $i < 10; $i++) {
+        echo $i . "<br>";
+    }
+
+    // FOR EACH LOOP (ARRAYS)
+
+    $numbers = ["somethng", 2, 3];
+    foreach ($numbers as $number) {
+        echo $number . "<br>";
+    }
+
+    ?>
+
+
 
 
 
