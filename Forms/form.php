d640dc46473561553f19b9e298eb54dbdaf1c970
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-06-02 12:08:34 +0300

    checking php forms

---
+++ Forms/form.php
@@ -0,0 +1,40 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    echo $username;
+    echo $password;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="form.php" method="post">
+
+        <input name="username" type="text" placeholder="Username">
+        <input name="password" type="password" placeholder="Password">
+        <br>
+        <input type="submit" name="submit">
+
+    </form>
+
+
+
+
+</body>
+
+</html>
